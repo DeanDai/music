48f712640d4fb9b1107db7fd0f0c958b8b62d873
Dean <huangangdai@91jkys.com>
2017-07-26 09:28:08 +0800

    git add header and radiolist

---
+++ src/components/RadioList.vue
@@ -0,0 +1,30 @@
+<template>
+	<mu-bottom-sheet :open="bottomSheet" @close="closeBottomSheet">
+    <mu-list @itemClick="closeBottomSheet">
+      <mu-sub-header>
+        请选择一个
+      </mu-sub-header>
+      <mu-list-item title="阴阳师"/>
+      <mu-list-item title="贪吃蛇大作战"/>
+      <mu-list-item title="一划到底"/>
+      <mu-list-item title="全民斗地主"/>
+    </mu-list>
+  </mu-bottom-sheet>
+</template>
+<script>
+	export default {
+		props: {
+			bottomSheet: false
+		},
+		methods: {
+			closeBottomSheet () {
+				
+			}
+		}
+	}
+</script>
+<style lang="less">
+	.recommand-radio {
+		padding: 0 12px;
+	}
+</style>
---
+++ src/components/SongInfoHeader.vue
@@ -0,0 +1,95 @@
+<template>
+	<div class="song-info">
+        <mu-list>
+          <mu-list-item :title="songInfo.songName" :describeText="songInfo.singerName">
+            <mu-avatar slot="left" :src="imgUrl"/>
+            <mu-icon class="icon_play" :value="icon_value" slot="right" @click="switchPlay" />
+          </mu-list-item>
+        </mu-list>
+      </div>
+</template>
+<script>
+	export default {
+		props:['songInfo', 'imgUrl'],
+		data () {
+			return {
+				playing: true	
+			}
+		},
+		computed: {
+			'icon_value' () {
+		      return !this.playing ? 'play_circle_outline' : 'pause_circle_outline';
+		    }
+		},
+		methods: {
+			switchPlay () {
+		      this.playing = !this.playing;
+		      this.$emit('play', this.playing);
+		    }
+		}
+	}
+</script>
+<style lang="less">
+	.mu-avatar {
+      height: 80px;
+      width: 80px;
+      img {
+        -webkit-animation: circling 20s infinite linear;
+        animation: circling 20s infinite linear;
+        @-webkit-keyframes circling {
+          0% {
+              -webkit-transform: rotate(0deg);
+              transform: rotate(0deg)
+          }
+
+          to {
+              -webkit-transform: rotate(1turn);
+              transform: rotate(1turn)
+          }
+        }
+
+        @keyframes circling {
+            0% {
+                -webkit-transform: rotate(0deg);
+                transform: rotate(0deg)
+            }
+
+            to {
+                -webkit-transform: rotate(1turn);
+                transform: rotate(1turn)
+            }
+        }
+      }
+    }
+    .song-info {
+      .mu-item-wrapper {
+        padding: 15px 0;
+        background: rgba(0, 0, 0, 0.1);  
+      }
+      .mu-item {
+        height: 80px;
+        color: #fff;
+        .mu-item-text {
+          color: #fff;
+        }
+        .mu-item-left {
+          width: 80px;
+          max-height: 80px;
+        }
+        .mu-item-right {
+          right: 24px;
+          max-height: inherit;
+        }
+        &.show-left {
+          padding-left: 120px;
+        }
+      }
+    }
+    .icon_play {
+      font-size: 60px;
+      color: rgba(255, 255, 255, .8);
+    }
+    .mu-ripple-wrapper {
+      display: none;
+    }
+</style>
